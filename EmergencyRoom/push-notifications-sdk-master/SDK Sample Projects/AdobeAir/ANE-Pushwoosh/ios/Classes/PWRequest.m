c2b9b5c12afb85d3ae122b625189f368d9687131
Marco Velluto <velluto93@gmail.com>
2013-08-18 14:22:09 +0200

    sync

---
+++ EmergencyRoom/push-notifications-sdk-master/SDK Sample Projects/AdobeAir/ANE-Pushwoosh/ios/Classes/PWRequest.m
@@ -0,0 +1,35 @@
+//
+//  PWRequest.m
+//  Pushwoosh SDK
+//  (c) Pushwoosh 2012
+//
+
+#import "PWRequest.h"
+
+@implementation PWRequest
+@synthesize appId, hwid;
+
+- (NSString *) methodName {
+	return @"";
+}
+
+//Please note that all values will be processed as strings
+- (NSDictionary *) requestDictionary {
+	return nil;
+}
+
+- (NSMutableDictionary *) baseDictionary {
+	NSMutableDictionary *dict = [NSMutableDictionary new];
+	[dict setObject:appId forKey:@"application"];
+	[dict setObject:hwid forKey:@"hwid"];
+	return [dict autorelease];
+}
+
+- (void) dealloc {
+	self.appId = nil;
+	self.hwid = nil;
+	
+	[super dealloc];
+}
+
+@end
